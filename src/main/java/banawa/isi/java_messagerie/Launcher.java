1e0f0df76a5587135ad4354f588e6930361e2973
latifandao <latifandao62@gmail.com>
2026-03-03 18:48:46 +0000

    premier commit

---
+++ src/main/java/banawa/isi/java_messagerie/Launcher.java
@@ -0,0 +1,9 @@
+package banawa.isi.java_messagerie;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module banawa.isi.java_messagerie {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.ikonli.javafx;
+
+    opens banawa.isi.java_messagerie to javafx.fxml;
+    exports banawa.isi.java_messagerie;
+}
